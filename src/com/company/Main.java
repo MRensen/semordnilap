b32b1db8e47049bdf92e984a841c1190c86e4779
MRensen <markrensen@hotmail.com>
2018-03-04 20:34:09 +0100

    optimalized with binary search. needs another binary search though

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-    static File file = new File("/home/rensen/IdeaProjects/semordnilap/src/com/company/words.txt");
+    static File file = new File("src/com/company/words.txt");
 
     public static void main(String[] args) {
         // Using only words from this list, report the total number of unique semordnilap pairs, and print 5 examples
@@ -15,17 +15,66 @@ public class Main {
         try {
             reader = new BufferedReader(new FileReader(file));
             String line;
-            ArrayList list = new ArrayList();
+            ArrayList<String> list = new ArrayList();
+            int counter = 0;
 
-            int test = 0;
+            ArrayList<String> checkList = new ArrayList<>();
 
             while ((line = reader.readLine()) != null) {
                 list.add(line);
             }
 
-            reverse(list);
+            for(int i = 0; i < list.size(); i++) {
+                String iReverse = new StringBuilder( list.get(i)).reverse().toString();
+                String iWord = list.get(i);
+                String lastLetter = iWord.substring((iWord.length()-1), iWord.length());
+                int max = list.size();
+                int min = 0;
+                int result = recursiveBinary(list, lastLetter, max, min);
 
+                if(result >=0 && result <= list.size()){
+                    int o = result;
+                    //System.out.println(" test: "  + "list.get(o) :" +  list.get(o) + ", iWord: " + iWord+", iReverse: " + iReverse);
+                    while(list.get(o).substring(0,1).equals(lastLetter)){
 
+                        if (list.get(o).equals(iWord)){
+
+                        } else if(list.get(o).equals(iReverse)){
+                            // checklist.contains should be exchanged for another binary search for optimalization
+                            if(!checkList.contains(iWord)) {
+                                System.out.println(list.get(o) + " - " + iWord);
+                                checkList.add(list.get(o));
+                            }
+                        }
+                        if(o>0){
+                            o--;
+                        } else{
+                            break;
+                        }
+                    }
+                    o=result;
+                    while(list.get(o).substring(0,1).equals(lastLetter)){
+
+                        if (list.get(o).equals(iWord)){
+
+                        } else if(list.get(o).equals(iReverse)){
+                            // checklist.contains should be exchanged for another binary search for optimalization
+                            if(!checkList.contains(iWord)) {
+                                System.out.println(list.get(o) + " - " + iWord);
+                                checkList.add(list.get(o));
+                            }                         }
+                        if(o<(list.size()-1)){
+                            o++;
+                        }else{
+                            break;
+                        }
+
+                    }
+                }else{
+                    System.out.println("failure: binary search result < 0");
+                }
+            }
+            System.out.println(checkList.size());
 
 
         } catch (IOException e) {
@@ -41,19 +90,33 @@ public class Main {
         }
     }
 
-    public static void reverse (ArrayList list){
+    public static int recursiveBinary (ArrayList<String> list, String key, int max, int min){
 
-        for(int i = 0; i < list.size(); i++){
-            String iReverse = new StringBuilder((String) list.get(i)).reverse().toString();
-            if(list.contains(iReverse)){
-                System.out.println( i + " - " + iReverse);
-                list.remove(iReverse);
-                list.remove(i);
-                if(list.size()>0){
-                    reverse(list);
-                }
-            }
+        int middle = (min + max)/2;
+
+        if(max<min){
+            return -1;
         }
-    }
 
+        if(key.compareTo((list.get(middle)).substring(0,1))==0){
+            return middle;
+        } else if(key.compareTo((list.get(middle)).substring(0,1)) < 0){
+            return recursiveBinary(list , key, middle-1, min);
+        } else{
+            return recursiveBinary(list, key, max, middle+1);
+        }
+
+
+//            if(list.contains(iReverse)){
+//                System.out.println( i + " - " + iReverse);
+//                list.remove(iReverse);
+//                list.remove(i);
+//                if(list.size()>0){
+//                    reverse(list, i);
+//                }
+//            }
+
+    }
 }
+
+
